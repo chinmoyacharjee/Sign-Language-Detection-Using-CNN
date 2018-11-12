2b69c8baec6b231570fa1b330b3e174590cb86eb
Chinmoy007 <chinmoyacharjee15@gmail.com>
2018-11-12 19:37:11 +0600

    re-organized, added 3 models

--- gesture_recognition.py
+++ gesture_recognition.py
@@ -5,138 +5,186 @@ from color_detection import RangeColorDetector
 from keras.models import load_model
 from keras.preprocessing import image
 
-#Firs image boundaries
-# min_range = np.array([0, 48, 70], dtype = "uint8") #lower HSV boundary of skin color
-# max_range = np.array([20, 150, 255], dtype = "uint8") #upper HSV boundary of skin color
+# -----------------Skin range---------------------
+#skin range
+#min_range = np.array([0, 58, 50], dtype = "uint8")         #lower HSV boundary of skin color
+#max_range = np.array([30, 255, 255], dtype = "uint8")      #upper HSV boundary of skin color
 
-min_range = np.array([0, 58, 50], dtype = "uint8") #lower HSV boundary of skin color
-max_range = np.array([30, 255, 255], dtype = "uint8") #upper HSV boundary of skin color
+min_range = np.array([0, 48, 70], dtype = "uint8")          #lower HSV boundary of skin color
+max_range = np.array([20, 150, 255], dtype = "uint8")       #upper HSV boundary of skin color
 
 my_skin_detector = RangeColorDetector(min_range, max_range) #Define the detector object
 
-classifier = load_model('digit_0_10_OP.h5')
-print("--> model loaded")
-#----------------------
+# -----------------Recntangle properties---------------------
+posx = 300
+posy = 10
+width = 300
+height = 300
 
-#----------------------
-#class_value_dict = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13,
-#                     'O': 14, 'P': 15, 'Q': 16, 'T': 17, 'U': 18, 'V': 19, 'W': 20, 'Y': 21, 'Z': 22}
+# -----------------frames counting for calculator---------------------
+frames_per_item = 30
+detection_item_threshold = .8
 
-class_value_dict = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '/': 10, '-': 11, '*': 12, '+': 13, '10': 14}
-class_value_arr = list(class_value_dict.keys())
+# -----------------Calculator window properties---------------------
+calc_screen = np.zeros((300, 500, 3), np.uint8)
+calc_screen[:] = (64, 55, 88)
 
+# -----------------Camera properties---------------------
+angle = 180
 
+# -----------------models---------------------
+calcultaor_model = "digit_0_10_OP.h5"
+digit_model = "digit_model_0_10.h5"
+alphabet_model = "alphabetA-Z.h5"
 
-#digit ={'0': 0, '1': 1, '2': 10, '3': 4, '4': 5, '5': 6, '6': 7, '7': 8, '8': 9, '9': 10, 'D9': 10}
+# -----------------models dictionaries---------------------
+calculator_dict = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '/': 10, '-': 11, '*': 12, '+': 13, '10': 14}
+digit_dict = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10}
+alphabet_dict = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13,
+                     'O': 14, 'P': 15, 'Q': 16, 'T': 17, 'U': 18, 'V': 19, 'W': 20, 'Y': 21, 'Z': 22}
 
+# -----------------font properties---------------------
+font = cv2.FONT_HERSHEY_SIMPLEX
+main_window_text_pos, calc_text_pos = (80,150), (10, 100)
+main_window_font_scale, calc_font_scale = 5, 1
+font_color = (255,255,255)
+main_window_line_type, calc_line_type = 10, 1
 
-
-#-------------------------
-
-font                   = cv2.FONT_HERSHEY_SIMPLEX
-bottomLeftCornerOfText = (80,150)
-fontScale              = 5
-fontColor              = (255,255,255)
-lineType               = 10
-
-
-#-------------------------
-
-
+# -----------------skin detect according to range defined avobe---------------------
 def return_skin(frame):
-    min_range = np.array([0, 58, 50], dtype = "uint8") #lower HSV boundary of skin color
-    max_range = np.array([30, 255, 255], dtype = "uint8") #upper HSV boundary of skin color
+    min_range = np.array([0, 58, 50], dtype = "uint8") 
+    max_range = np.array([30, 255, 255], dtype = "uint8") 
     image = frame 
-    my_skin_detector.setRange(min_range, max_range)
-    #For this image we use one iteration of the morph_opening and gaussian blur to clear the noise
+    my_skin_detector.setRange(min_range, max_range)    
     only_skin = my_skin_detector.returnFiltered(image, morph_opening=True, blur=True, kernel_size=3, iterations=1)
     binary_filtered = my_skin_detector.returnMask(image, morph_opening=True, blur=True, kernel_size=3, iterations=1)
-    return only_skin, binary_filtered #Save the filtered image
+    return only_skin, binary_filtered 
 	
-
+# -----------------Frame Rotation - [camera wise]---------------------
 def rotateImage(image, angle):
     image_center = tuple(np.array(image.shape[1::-1]) / 2)
     rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
     result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
     return result
 
-
-
-posx, posy, width, height = 300, 10, 300, 300
-	
-cap = cv2.VideoCapture("http://192.168.0.103:4747/video", 0)
-cap.set(cv2.CAP_PROP_FPS, 6000)
-
-predicted_text = ""
-frame_captured = 0
-predicted_res_arr = []
-
-
-frames_per_item = 30
-detection_item_threshold = .8
-
-
-finel_text_image = np.zeros((300, 400, 3), np.uint8)
-finel_text_image[:] = (64, 55, 88)
-
-while cap.isOpened():
-    
-    _, frame = cap.read()
-    #    frame = cv2.flip(frame, 1)  
+# -----------------Operation Properties---------------------
+class Operation(object):
+    def __init__(self, model, dictionary, calc = False):
+        self.model = model
+        self.model_class_dictionary = dictionary
+        self.calc = calc
+        """
+        model = which model to load
+        model_class_dictionary = model wise dictionary ds
+        calc = is operation is calcultor using sign language
+        """
         
-    frame = rotateImage(frame, 180)
-
-    cv2.rectangle(frame, (posx,posy), (posx+width, posy+height), (45, 55, 188), 1)
-    img = frame[posy:posy+height, posx:posx+width]
-
-    only_skin, binary_filtered = return_skin(img)
+# -----------------Capture Func---------------------        
+def capture(camera_index, op):
     
-    contours = cv2.findContours(binary_filtered.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
+    classifier = load_model(op.model)
+    print("--> model loaded")
     
-    if len(contours) > 0:
-        contour = max(contours, key = cv2.contourArea)
-        if(cv2.contourArea(contour) > 10000):
-            
-            cv2.imwrite('saved_image.jpg',  cv2.resize(binary_filtered,(80,80)))
+    class_value_arr = list(op.model_class_dictionary.keys())
+    	
+    cap = cv2.VideoCapture(camera_index, 0)
+#    cap.set(cv2.CAP_PROP_FPS, 6000)
     
-            test_image = image.load_img('saved_image.jpg', target_size = (80, 80))
-            test_image = image.img_to_array(test_image)
-            test_image = np.expand_dims(test_image, axis = 0)
-            result = classifier.predict(test_image)
-
-            _, j = np.unravel_index(result.argmax(), result.shape)
-            max_value_index = j
-            
-            pred = class_value_arr[max_value_index]          
-            
-            predicted_res_arr.append(pred)
-            
-            if(frame_captured >= frames_per_item):
-                
-                max_time_occured_item = max(predicted_res_arr, key = predicted_res_arr.count)
-                max_time_occured_item_occurences = predicted_res_arr.count(max_time_occured_item)
-                
-                prob = max_time_occured_item_occurences / frames_per_item
-                
-                if(prob >= detection_item_threshold):
-                    predicted_text += max_time_occured_item
-                    predicted_res_arr = []
-                    frame_captured = 0
-                
-            
-            cv2.putText(frame, str(pred), bottomLeftCornerOfText, font, fontScale, fontColor, lineType)
-            cv2.putText(finel_text_image, str(predicted_text), (10, 100), font, 1, fontColor, 3)
-            
-#            print("Predicted result is: {}, {}".format(max_value_index, pred))
-            frame_captured += 1    
+    predicted_text = ""
+    frame_captured = 0
+    predicted_res_arr = []
+    
+    while cap.isOpened():
         
-    cv2.imshow("Orginal", frame)
-    cv2.imshow("Only Skin", only_skin)
-    cv2.imshow("Binary Skin", binary_filtered)
-    cv2.imshow("Final",finel_text_image)
+        _, frame = cap.read()
+        #    frame = cv2.flip(frame, 1)  
+            
+        frame = rotateImage(frame, angle)
     
-    if cv2.waitKey(1) == ord('q'):
-        cv2.destroyAllWindows()
-        break
+        cv2.rectangle(frame, (posx,posy), (posx+width, posy+height), (45, 55, 188), 1)
+        img = frame[posy:posy+height, posx:posx+width]
+    
+        only_skin, binary_filtered = return_skin(img)
+        
+        contours = cv2.findContours(binary_filtered.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
+        
+        if(len(contours) > 0):
+            contour = max(contours, key = cv2.contourArea)
+            if(cv2.contourArea(contour) > 10000):
+                
+                cv2.imwrite('temp.jpg',  cv2.resize(binary_filtered,(80,80)))
+        
+                test_image = image.load_img('temp.jpg', target_size = (80, 80))
+                test_image = image.img_to_array(test_image)
+                test_image = np.expand_dims(test_image, axis = 0)
+                result = classifier.predict(test_image)
+    
+                _, j = np.unravel_index(result.argmax(), result.shape)
+                max_value_index = j
+                
+                pred = class_value_arr[max_value_index]          
+                
+                predicted_res_arr.append(pred)
+                
+                if(op.calc and frame_captured >= frames_per_item):
+                    
+                    max_time_occured_item = max(predicted_res_arr, key = predicted_res_arr.count)
+                    max_time_occured_item_occurences = predicted_res_arr.count(max_time_occured_item)
+                    
+                    prob = max_time_occured_item_occurences / frames_per_item
+                    
+                    if(prob >= detection_item_threshold):
+                        predicted_text += max_time_occured_item
+                        predicted_res_arr = []
+                        frame_captured = 0                   
+                    
+                
+                cv2.putText(frame, str(pred), main_window_text_pos, font, main_window_font_scale, fontColor, lineType)
+                
+                if(op.calc):
+                    cv2.putText(calc_screen, str(predicted_text), calc_text_pos, font, calc_font_scale, fontColor, 3)
+    
+                frame_captured += 1    
+            
+        cv2.imshow("Orginal", frame)
+        cv2.imshow("Only Skin", only_skin)
+        cv2.imshow("Binary Skin", binary_filtered)
+        if(op.calc): cv2.imshow("Calculator Screen", calc_screen)
+         
+        if cv2.waitKey(1) == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+
+def manage(camera_index = "http://192.168.0.100:4747/video", operation = "-d"):
+    model = ""
+    op_dict = {}
+    
+    if(operation == "-c"):
+        model, op_dict, is_calc = calcultaor_model, calculator_dict, True
+    elif(operation == "-d"):
+        model, op_dict, is_calc = digit_model, digit_dict, False
+    elif(operation == "-a"):
+        model, op_dict, is_calc = alphabet_model, alphabet_dict, False
+        
+    op = Operation(model, op_dict, is_calc)
+    capture(camera_index, op)
+    
+    
+    
+if(__name__ == "__main__"):
+    manage()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
